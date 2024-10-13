d55bc61787746b88bc4ff31ab2238fcc9279fa8f
sameyol <73040492+sameyol@users.noreply.github.com>
2024-10-13 16:51:53 +0100

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
+import TopNav from '../../Components/Navbars/TopNav';
 import Desktop from './Desktop';
 import Mobile from './Mobile';
+
 
 const Home = () => {
 	return (
 		<section className='w-full'>
 			<div className='lg:flex'>
+				<TopNav />
 				<Desktop />
 			</div>
 			<div className='flex lg:hidden'>
