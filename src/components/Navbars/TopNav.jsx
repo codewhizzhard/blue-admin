43250b61acb5ba2bd233c53853be4da85e4530d3
sameyol <73040492+sameyol@users.noreply.github.com>
2024-11-17 10:54:01 +0100

    Update TopNav.jsx

--- src/components/Navbars/TopNav.jsx
+++ src/components/Navbars/TopNav.jsx
@@ -1,155 +1,138 @@
-import { Link } from 'react-router-dom';
-import { GrCircleInformation } from 'react-icons/gr';
-import { AiOutlineCustomerService } from 'react-icons/ai';
-import { MdWorkspacePremium } from 'react-icons/md';
-import { HiOutlineStatusOnline } from 'react-icons/hi';
-import { IoMdNotificationsOutline } from 'react-icons/io';
-import { FaRegEnvelope } from 'react-icons/fa6';
-import { FiSearch } from 'react-icons/fi';
-import { RiDeleteBin6Line } from 'react-icons/ri';
-import { PiUserCircleLight } from 'react-icons/pi';
-import Logo from '../../assets/white_bg_logo.svg';
-import { FaChevronUp, FaChevronDown } from 'react-icons/fa';
-import { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
+import { GrCircleInformation } from "react-icons/gr";
+import { AiOutlineCustomerService } from "react-icons/ai";
+import { MdWorkspacePremium } from "react-icons/md";
+import { HiOutlineStatusOnline } from "react-icons/hi";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { FaRegEnvelope, FaChevronUp, FaChevronDown } from "react-icons/fa";
+import { FiSearch } from "react-icons/fi";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { PiUserCircleLight } from "react-icons/pi";
+import { useState } from "react";
+import { useAuthContext } from "../../../hooks/useAuthContext";
+import Logo from "../../assets/white_bg_logo.svg";
+
 const TopNav = () => {
-	const [dropdown, setDropDown] = useState(false);
+  const { user } = useAuthContext();
+  const [dropdown, setDropDown] = useState(false);
 
-	const [data, setData] = useState('');
-	// const navigate = useNavigate();
-	useEffect(() => {
-		if (localStorage.getItem('user')) {
-			const foundUser = JSON.parse(localStorage.getItem('user'));
-			setData(foundUser.user);
-		}
-	}, []);
+  const initials = user?.user
+    ? `${user.user.firstName[0]}${user.user.surName[0]}`
+    : "SR";
 
-	const { surName, firstName } = data;
+  const fullName = user?.user
+    ? `${user.user.firstName} ${user.user.surName}`
+    : "Stylish Racon";
 
-	return (
-		<nav className=' z-50 fixed topnav  bg-white w-full h-[67px] py-[10px] px-[5px]  shadow-bg shadow resize-none'>
-			<header className='flex flex-row items-center justify-between h-full w-full px-4'>
-				<Link to={'/'}>
-					<img src={Logo} alt='Logo' className='w-[82px]' />
-				</Link>
+  return (
+    <nav className="z-50 fixed bg-white w-full h-[67px] py-2 px-5 shadow-md">
+      <header className="flex items-center justify-between h-full">
+        {/* Logo */}
+        <Link to="/" aria-label="Home">
+          <img src={Logo} alt="Logo" className="w-[82px]" />
+        </Link>
 
-				<div className='flex flex-row items-center justify-between w-full gap-[3rem]'>
-					{/* Search Bar */}
-					<div className='flex items-center justify-between ml-[8rem] mr-[3rem] h-[40px] bg-bg py-[10px] px-[16px] rounded-[7px] flex-grow'>
-						<span className='text-mediumGray text-[14px]'>
-							<FiSearch />
-						</span>
-						<input
-							type='text'
-							placeholder='Search'
-							className='bg-transparent flex-grow h-full text-[14px] focus:outline-none ml-2' // Added ml-2 for spacing
-						/>
-					</div>
+        {/* Search Bar */}
+        <div className="flex-grow ml-[8rem] mr-[3rem] flex items-center bg-bg h-[40px] rounded-[7px] px-4">
+          <FiSearch className="text-mediumGray" aria-hidden="true" />
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-transparent flex-grow text-[14px] focus:outline-none ml-2"
+          />
+        </div>
 
-					{/* Icons & Profile */}
-					<div className='flex items-center justify-end gap-[1rem]'>
-						<ul className='flex flex-row gap-[16px] items-center'>
-							<Link to={'/'} className='text-primaryGreen'>
-								<span>
-									<HiOutlineStatusOnline />
-								</span>
-							</Link>
-							<Link to={'/'} className='text-mediumGray'>
-								<span>
-									<FaRegEnvelope />
-								</span>
-							</Link>
-							<Link to={'/'} className='text-mediumGray'>
-								<span>
-									<IoMdNotificationsOutline />
-								</span>
-							</Link>
-						</ul>
+        {/* Icons and Profile */}
+        <div className="flex items-center gap-4">
+          <ul className="flex items-center gap-4">
+            <li>
+              <Link to="/" className="text-primaryGreen" aria-label="Status">
+                <HiOutlineStatusOnline />
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/"
+                className="text-mediumGray"
+                aria-label="Messages"
+              >
+                <FaRegEnvelope />
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/"
+                className="text-mediumGray"
+                aria-label="Notifications"
+              >
+                <IoMdNotificationsOutline />
+              </Link>
+            </li>
+          </ul>
 
-						{/* Profile & Dropdown */}
-						<div className='flex items-center gap-2'>
-							<div className='w-[35px] h-[35px] flex justify-center items-center rounded-full bg-primaryGreen'>
-								<span className='text-[14px] font-Outfit font-bold text-white'>
-									{firstName ? firstName[0] + surName[0] : 'SR'}
-								</span>
-							</div>
-							<button
-								className='font-Outfit font-medium text-[14px] text-textColor flex gap-2 items-center'
-								onClick={() => setDropDown(!dropdown)}>
-								{firstName ? `${firstName}  ${surName} ` : 'Stylish Racon'}
-								{dropdown ? <FaChevronUp /> : <FaChevronDown />}
-							</button>
+          {/* Profile & Dropdown */}
+          <div className="relative">
+            <button
+              className="flex items-center gap-2"
+              onClick={() => setDropDown(!dropdown)}
+              aria-expanded={dropdown}
+              aria-controls="profile-dropdown"
+            >
+              <div className="w-[35px] h-[35px] flex items-center justify-center rounded-full bg-primaryGreen">
+                <span className="text-white font-bold text-[14px]">{initials}</span>
+              </div>
+              <span className="text-textColor font-medium text-[14px]">
+                {fullName}
+              </span>
+              {dropdown ? <FaChevronUp /> : <FaChevronDown />}
+            </button>
 
-							{/* Dropdown Menu */}
-							<div className='relative'>
-								<div
-									className={`absolute shadow-xl z-50 rounded-[8px] flex flex-col
-										 p-[10px] bg-white -right-5 w-[238px] h-[290px] transition-all justify-center items-center ${
-												dropdown ? 'top-[2.2rem]' : '-top-[30rem]'
-											}`}>
-									<ul className='dropDown flex flex-col w-full  gap-[16px]'>
-										<li>
-											<Link to={'/'}>
-												{' '}
-												<span>
-													<PiUserCircleLight />
-												</span>
-												<span>My Profile</span>{' '}
-											</Link>
-										</li>
-										<li>
-											<Link to={'/'}>
-												{' '}
-												<span>
-													<GrCircleInformation />
-												</span>
-												<span>About</span>{' '}
-											</Link>
-										</li>
-										<li>
-											<Link to={'/'}>
-												{' '}
-												<span>
-													<AiOutlineCustomerService />
-												</span>
-												<span>Customer support</span>{' '}
-											</Link>
-										</li>
-										<li>
-											<Link to={'/'}>
-												{' '}
-												<span>
-													<MdWorkspacePremium />
-												</span>
-												<span>Premium</span>{' '}
-											</Link>
-										</li>
-										<li>
-											<Link to={'/'}>
-												{' '}
-												<span>
-													<MdWorkspacePremium />
-												</span>
-												<span>Survey</span>{' '}
-											</Link>
-										</li>
-										<li>
-											<Link to={'/'}>
-												{' '}
-												<span>
-													<RiDeleteBin6Line />
-												</span>
-												<span>LogOut</span>{' '}
-											</Link>
-										</li>
-									</ul>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</header>
-		</nav>
-	);
+            {/* Dropdown Menu */}
+            <div
+              id="profile-dropdown"
+              className={`absolute right-0 mt-2 bg-white shadow-xl rounded-[8px] transition-all ${
+                dropdown ? "block" : "hidden"
+              }`}
+              role="menu"
+            >
+              <ul className="flex flex-col gap-3 p-4">
+                <li>
+                  <Link to="/" className="flex items-center gap-2">
+                    <PiUserCircleLight />
+                    <span>My Profile</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/" className="flex items-center gap-2">
+                    <GrCircleInformation />
+                    <span>About</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/" className="flex items-center gap-2">
+                    <AiOutlineCustomerService />
+                    <span>Customer Support</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/" className="flex items-center gap-2">
+                    <MdWorkspacePremium />
+                    <span>Premium</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/" className="flex items-center gap-2">
+                    <RiDeleteBin6Line />
+                    <span>Logout</span>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </header>
+    </nav>
+  );
 };
 
 export default TopNav;
