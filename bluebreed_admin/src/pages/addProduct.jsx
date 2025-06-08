daa1e9884b6b248d90fb8ee5d7676092d6b6ea6a
codewhizzhard <topeadeolu5@gmail.com>
2025-06-08 23:54:52 +0100

    added

--- bluebreed_admin/src/pages/addProduct.jsx
+++ bluebreed_admin/src/pages/addProduct.jsx
@@ -3,6 +3,8 @@ import { FiX } from 'react-icons/fi'
 
 const AddProduct = () => {
     const [clickStates, setClickedStates] = useState({btn1: false, btn2: false});
+    const [inputTagState, setInputTagState] = useState({input1: "", input2: ""});
+    const [tagsState, setTagsState] = useState({tag1: [], tag2: []});
 
     const handleClick = (btn) => {
         setClickedStates((prev) => ({
@@ -10,6 +12,18 @@ const AddProduct = () => {
             [btn]: !prev[btn]
         }))
     }
+
+    const handleEnterTags = (e, tag) => {
+
+        if ((e.key === "Enter" || e.key === ",") && inputTagState[tag].trim()) {
+            e.preventDefault();
+            const newTag = inputTagState[tag].trim().replace(/,$/, '');
+            if (!tagsState[tag].includes(newTag)) {
+                setTagsState[tag]([...tag, newTag])
+            }
+        }
+    }
+
   return (
     <div className='flex w-full h-full mb-4 space-x-3 '>
         <form className='flex-grow p-5 bg-white rounded-[6px] mb-4 space-y-4'>
@@ -41,7 +55,7 @@ const AddProduct = () => {
             </div>
             {/*  */}
             <div className='space-y-3'>
- button               <h3 className='text-[16px] text-[#131523] font-bold'>Price</h3>
+               <h3 className='text-[16px] text-[#131523] font-bold'>Price</h3>
                 <div className='flex w-full justify-between'>
                     <div className='flex flex-col w-[47%]'>
                     <label htmlFor="" className='text-[14px] text-[#5A607F]'>Product Price</label>
@@ -53,7 +67,7 @@ const AddProduct = () => {
                     </div>
                 </div>
                 <div className='flex gap-2'>
-                    <button className={`w-11 h-6 rounded-2xl relative ${clickStates.btn1 ? "bg-[#E6B566]" : "bg-[#FF89011A] "}`} onClick={() => handleClick("btn1")} type='button'><span className={`bg-white absolute w-4 h-4 rounded-xl bottom-1 ${clickStates.btn1 ? "right-1" : "left-1"} `} style={{filter: "drop-shadow(0 4px 6px rgba(38, 44, 71, 0.16))"}}></span></button>{/* shadow-[#262C4729] */}
+                    <button className={`w-11 h-6 rounded-2xl relative ${clickStates.btn1 ? "bg-[#E6B566]" : "bg-[#FF89011A] "}`} onClick={() => handleClick("btn1")} type='button'><span className={`bg-white absolute w-4 h-4 rounded-xl bottom-1 ${clickStates.btn1 ? "right-1" : "left-1"} `} ></span></button>{/* shadow-[#262C4729] */}
                     <p className='text-[#131523] text-[16px]'>Include VAT or Custom Charges?</p>
                 </div>
             </div>
@@ -61,7 +75,7 @@ const AddProduct = () => {
             <div className='space-y-3'>
                 <h3 className='text-[#131523] text-[16px] font-bold'>Different Options</h3>
                 <div className='flex gap-2'>
-                    <button className={`w-11 h-6 rounded-2xl relative ${clickStates.btn2 ? "bg-[#E6B566]" : "bg-[#FF89011A] "}`} onClick={() => handleClick("btn2")} type='button'><span className={`bg-white absolute w-4 h-4 rounded-xl bottom-1 ${clickStates.btn2 ? "right-1" : "left-1"} `} /* style={{filter: "drop-shadow(0 4px 6px rgba(38, 44, 71, 0.16))"}} */></span></button>{/* shadow-[#262C4729] */}
+                    <button className={`w-11 h-6 rounded-2xl relative ${clickStates.btn2 ? "bg-[#E6B566]" : "bg-[#FF89011A] "}`} onClick={() => handleClick("btn2")} type='button'><span className={`bg-white absolute w-4 h-4 rounded-xl bottom-1 ${clickStates.btn2 ? "right-1" : "left-1"} `} ></span></button>{/* shadow-[#262C4729] */}
                     <p className='text-[#131523] text-[16px]'>Does this product have size or material variations?</p>
                 </div>
             </div>
@@ -70,9 +84,9 @@ const AddProduct = () => {
                 <h3 className='text-[#131523] text-[16px] font-bold'>Size / Dimensions</h3>
                 <div className='space-y-1 flex flex-col'>
                     <label htmlFor="" className='text-[14px] text-[#5A607F]'>Value</label>
-                    <div className=' flex h-10 items-center divx-2 rounded border border-[#D9E1EC]'>
+                    <div className=' flex h-10 items-center divx-2 rounded border border-[#D9E1EC] px-2'>
                     <div className=' flex gap-2 h-[80%]'><span className='py-1 px-3 rounded flex items-center bg-[#E6E9F4] w-fit gap-2 text-[#5A607F] text-[14px]'>SM <FiX className='h-fullpt-1 text-[#7E84A3]'/></span> <span className='py-1 px-3 rounded flex items-center bg-[#E6E9F4] w-fit gap-2 text-[#5A607F] text-[14px]'>LG <FiX className='h-fullpt-1 text-[#7E84A3]'/></span> <span className='py-1 px-3 rounded flex items-center bg-[#E6E9F4] w-fit gap-2 text-[#5A607F] text-[14px]'>MD <FiX className='h-fullpt-1 text-[#7E84A3]'/></span></div>
-                    <input type="text" className='w-full px-3 text-[#A1A7C4] text-[16px] h-[80%]  outline-none' placeholder=''/></div>
+                    <input type="text" className='w-full px-3 text-[#A1A7C4] text-[16px] h-[80%]  outline-none' placeholder='' value={inputTagState.input1} onChange={(e) => setInputTagState((prev) => ({...prev, input1: e.target.value}))}/></div>
                  </div>
             </div>
 
@@ -94,7 +108,7 @@ const AddProduct = () => {
                 <h3 className='text-[16px] font-bold text-[#131523]'>Tags</h3>
                 <div className=' text-[#131523] text-[16px] gap-2 '>
                     <label htmlFor="">Add Tags</label>
-                    <input type="text" className='w-full h-10 border border-[#D7DBEC] rounded text-[14px] px-2 outline-none mt-1' placeholder='Add Fabric Type, Style, or Material'/>
+                    <input type="text" className='w-full h-10 border border-[#D7DBEC] rounded text-[14px] px-2 outline-none mt-1' placeholder='Add Fabric Type, Style, or Material' value={inputTagState.input2} onChange={(e) => setInputTagState((prev) => ({...prev, input2: e.target.value}))}/>
                 </div>
                 <div className='flex-wrap flex gap-2'>
                     <span className='py-1 px-3 rounded flex items-center bg-[#E6E9F4] w-fit gap-2 text-[#5A607F] text-[14px]'>nothing <FiX className='h-fullpt-1 text-[#7E84A3]'/></span>
