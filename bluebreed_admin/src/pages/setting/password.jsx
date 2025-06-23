f9ab6860e32430e178b6f83352435cdf3178c423
codewhizzhard <topeadeolu5@gmail.com>
2025-06-23 04:26:12 +0100

    settings page added

--- bluebreed_admin/src/pages/setting/password.jsx
+++ bluebreed_admin/src/pages/setting/password.jsx
@@ -1,19 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { GiEyelashes } from "react-icons/gi";
+
+
+
 
 const Password = () => {
+
+  const [viewPassword, setViewPassword] = useState({old: false, new: false, repeat: false});
+
+  const handleViewPassword = (state) => {
+    setViewPassword((prev) => ({
+      ...prev, [state]: !prev[state]
+    }))
+  }
   return (
     <form  className='rounded-[17px] border border-[#E1ECE3] p-4 space-y-2'>
       <div className='flex flex-col space-y-2'>
           <label htmlFor="old" className='text-[16px] font-medium'>Old Password</label>
-          <input type="password" className='rounded-[8px] border border-[#DFE8E1] text-[#777777] text-[14px] font-light p-3 outline-none' placeholder='John Doe' id='old'/>
+          <div className='relative w-full cursor-pointer'>
+            <input type={`${viewPassword.old ? 'type' : "password"}`} className='rounded-[8px] border border-[#DFE8E1] text-[#777777] text-[14px] font-light p-3 outline-none w-full' placeholder='John Doe' id='old'/>
+            {
+              !viewPassword.old ? <GiEyelashes className='absolute right-5 top-4' onClick={() => handleViewPassword("old")}/> : null
+            }
+            
+          </div>
+
       </div>
       <div className='flex flex-col space-y-2'>
           <label htmlFor="new" className='text-[16px] font-medium'>New Password</label>
-          <input type="password" className='rounded-[8px] border border-[#DFE8E1] text-[#777777] text-[14px] font-light p-3 outline-none' placeholder='John Doe' id='new'/>
+          <div className='relative w-full cursor-pointer'>
+            <input type={`${viewPassword.new ? 'type' : "password"}`} className='rounded-[8px] border border-[#DFE8E1] text-[#777777] text-[14px] font-light p-3 outline-none w-full' placeholder='John Doe' id='new'/>
+            {
+              !viewPassword.new ? <GiEyelashes className='absolute right-5 top-4' onClick={() => handleViewPassword("new")}/> : null
+            }
+          </div>
+          
       </div>
       <div className='flex flex-col space-y-2'>
           <label htmlFor="repeat" className='text-[16px] font-medium'>Repeat Password</label>
-          <input type="password" className='rounded-[8px] border border-[#DFE8E1] text-[#777777] text-[14px] font-light p-3 outline-none' placeholder='John Doe' id='repeat'/>
+           <div className='relative w-full cursor-pointer'>
+            <input type={`${viewPassword.repeat ? 'type' : "password"}`} className='rounded-[8px] border border-[#DFE8E1] text-[#777777] text-[14px] font-light p-3 outline-none w-full' placeholder='John Doe' id='repeat'/>
+            {
+              !viewPassword.repeat ? <GiEyelashes className='absolute right-5 top-4' onClick={() => handleViewPassword("repeat")}/> : null
+            }
+           </div>
+          
       </div>
     </form>
   )
